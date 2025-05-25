f251b0e1f3fbede19aeebbfb81ff94753a6e90eb
RodriguesCarol <rodriguesdemagalhaes@gmail.com>
2025-05-25 17:46:48 -0300

    Trabalho Final

---
+++ trabalho.js
@@ -0,0 +1,70 @@
+function geradorDeTagsDeIdentificacao(nome) {
+    return nome.toUpperCase()
+
+}
+
+
+function verificarSePodeSerAdotado(idadeDog, porte) {
+    const podeSerAdotado = idadeDog == 1 && porte === 'M' ? true : false
+    return podeSerAdotado
+}
+
+
+
+function calcularConsumoDeRacao(nome, idade, peso) {
+    const gramasPorDia = peso * 300
+    return gramasPorDia
+
+}
+
+
+
+
+let atividade
+function decidirTipoDeAtividadePorPorte(porte) {
+    switch (porte) {
+
+        case 'pequeno':
+
+            atividade = `brincar dentro de casa`
+
+            break
+
+        case 'médio':
+
+            atividade = `caminhada no quarteirão`
+
+            break
+
+        case 'grande':
+
+            atividade = `correr no parque`
+
+            break
+
+        default:
+
+            atividade = `porte inválido`
+
+    }
+
+    return atividade
+
+}
+
+
+
+async function buscarDadoAsync(resultado, nome) {
+    return await Promise.resolve('Pipoca');
+}
+
+
+
+
+export {
+    geradorDeTagsDeIdentificacao,
+    verificarSePodeSerAdotado,
+    calcularConsumoDeRacao,
+    decidirTipoDeAtividadePorPorte,
+    buscarDadoAsync
+}
